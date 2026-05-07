fec33bb2cf86ac5fcbb54f048925f06b3ff2d3d8
192425088simats <192425088.simats@saveetha.com>
2026-05-07 15:16:24 +0530

    Add files via upload

---
+++ EX.17 R CORRELATION ANALYSIS.R
@@ -0,0 +1,5 @@
+library(readxl)
+
+diabetest1 <- read_excel("C:/Users/Hema Karri/AppData/Local/Packages/5319275A.WhatsAppDesktop_cv1g1gvanyjgm/LocalState/sessions/0A5B51BCC2D5A34589BF54024A7ADED4D8197E51/transfers/2026-19/NARA.xlsx")
+
+colnames(diabetest1)
