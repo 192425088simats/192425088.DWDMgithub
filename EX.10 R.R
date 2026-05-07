fc40370d72a14774194988e6000380dedb2e08a4
192425088simats <192425088.simats@saveetha.com>
2026-05-07 15:01:38 +0530

    Add files via upload

---
+++ EX.10 R.R
@@ -0,0 +1,10 @@
+x<-as.integer(readline(prompt="enter the first number:"))
+
+z<-as.integer(readline(prompt="enter the third number:"))
+if(x>y&&x>x){
+  print(paste("greatest is:",x))
+}elseif(y>z){
+  print(paste("greatest is:",y))
+}else{
+  print(paste("greatest is:",z))
+}
